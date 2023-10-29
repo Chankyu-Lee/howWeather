c7eee2c7d20406a6e0157e454db33188e7210fa3
Lee GyeongJu <rudwn969@naver.com>
2023-10-29 15:40:58 +0900

    검색바 껍데기 아직 기능 X

--- src/main/java/org/howWeather/Search.java
+++ src/main/java/org/howWeather/Search.java
@@ -1,6 +1,7 @@
 package org.howWeather;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,6 +14,6 @@ public class Search implements ActionListener {
     public void actionPerformed(ActionEvent e){ VisibleSearch(); }
 
     public void VisibleSearch(){
-        search.mapLbl.setIcon(new ImageIcon("C:/Users/rudwn/Desktop/Blue Archive/디지털 굳즈/활륜대제 후야제.png"));
+
     }
 }
