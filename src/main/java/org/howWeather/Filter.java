e3266b07e46891b0389d5a1a9b27004325e13d51
Lee GyeongJu <rudwn969@naver.com>
2023-11-05 23:55:56 +0900

    FilterGroup 클래스 추가로 인한 코드 수정

--- src/main/java/org/howWeather/Filter.java
+++ src/main/java/org/howWeather/Filter.java
@@ -6,10 +6,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class Filter implements ActionListener {
-    Frame filter;
+public class Filter extends JPanel implements ActionListener {
 
-    public Filter(Frame filter){ this.filter = filter; }
+    public Filter(){
+        VisibleFilter();
+    }
 
     public JCheckBox mkCheckBox(JPanel p, String s){
         JCheckBox c = new JCheckBox(s);
@@ -20,32 +21,23 @@ public class Filter implements ActionListener {
         return c;
     }
 
-    public void actionPerformed(ActionEvent e){ VisibleFilter(); }
-
     public void VisibleFilter(){
-        JPanel filterPan = new JPanel();
-        filterPan.setLayout(new BoxLayout(filterPan, BoxLayout.Y_AXIS));
-        filterPan.setBackground(Color.white);
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setBackground(Color.white);
+        this.setPreferredSize(new Dimension(350,800));
 
-        JPanel themeChecks = new JPanel();
-        themeChecks.setBackground(Color.white);
-        themeChecks.setLayout(new FlowLayout(FlowLayout.LEFT));
+        JButton applyBtn = new JButton("필터 적용");
+        applyBtn.addActionListener(this);
 
-        JCheckBox themeCheck_culture = mkCheckBox(themeChecks,"문화/예술");
-        JCheckBox themeCheck_shopping = mkCheckBox(themeChecks,"쇼핑/놀이");
-        JCheckBox themeCheck_nature = mkCheckBox(themeChecks,"자연/힐링");
-        JCheckBox themeCheck_tradition = mkCheckBox(themeChecks,"종교/역사/전통");
-        JCheckBox themeCheck_practice = mkCheckBox(themeChecks,"체험/학습/산업");
-        JCheckBox themeCheck_camping = mkCheckBox(themeChecks,"캠핑/스포츠");
+        FilterGroup themeGrp = new FilterGroup("테마",new String[]{"문화/예술","쇼핑/놀이","자연/힐링","종교/역사/전통","체험/학습/산업","캠핑/스포츠"});
+        FilterGroup locateGrp = new FilterGroup("지역",new String[]{"수도권","강원","충청","경상","전라"});
+        this.add(locateGrp);
+        this.add(themeGrp);
 
-        //JButton closeBtn = new JButton("X");
-        //filterPan.add(closeBtn);
-        filterPan.add(new JLabel("테마"));
-        filterPan.add(themeChecks);
+        this.add(applyBtn);
+    }
 
-        filter.frameCnt.add(BorderLayout.CENTER,filterPan);
+    public void actionPerformed(ActionEvent e){
 
-        filter.frameCnt.validate();
-        filter.frameCnt.repaint();
     }
 }
