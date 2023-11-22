fa3bc5ffede63f46ce8d0bc5f78eb38699b4373a
Lee Gyeongju <rudwn969@naver.com>
2023-11-23 00:47:24 +0900

    코스들 -> 각 관광지 정보로 가는 스택 구조 구현

--- src/main/java/org/howWeather/Search.java
+++ src/main/java/org/howWeather/Search.java
@@ -4,33 +4,71 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Stack;
 
-public class Search extends JPanel {
+public class Search extends JPanel implements ActionListener {
 
-    Frame motherFrm;
-    JTextField searchFld = new JTextField(20);
-    JButton searchBtn = new JButton("검색");                      // JFrame 안쪽 영역에 들어갈 클릭 버튼
+    private Frame motherFrm;
+    private JTextField searchFld = new JTextField(20);
+    private JButton searchBtn = new JButton("검색");
+    private JPanel infoPnl;
+    private Stack<JPanel> infoPnlStack = new Stack<>();
 
     public Search(Frame frm){
         motherFrm = frm;
-        searchBtn.addActionListener(new SearchEvent());
+        setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+        searchBtn.addActionListener(this);
         VisibleSearch();
     }
 
     public void VisibleSearch(){
-        add(searchFld);
-        add(searchBtn);
+        JPanel searchPnl = new JPanel();
+
+        searchPnl.add(searchFld);
+        searchPnl.add(searchBtn);
+
+        add(searchPnl);
 
         setVisible(true);
     }
 
-    private class SearchEvent implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e){
-            if(!(searchFld.getText().isEmpty())){
-                motherFrm.filter.unckeckAll();
-                //NaverMap2.map_service(motherFrm, Long.parseLong(searchFld.getText()));
-            }
+    public void pushinfoPnl(JPanel pnl){
+        infoPnlStack.push(pnl);
+        drawInfoPnl();
+    }
+
+    public void popinfoPnl(){
+        infoPnlStack.pop();
+    }
+
+    public void drawInfoPnl(){
+        if(infoPnl != null){
+            clearinfoPnl();
+        }
+        infoPnl = infoPnlStack.peek();
+        add(infoPnl);
+        motherFrm.refresh();
+    }
+
+    public void clearinfoPnl(){
+        remove(infoPnl);
+        infoPnl = null;
+        motherFrm.refresh();
+        if(!(infoPnlStack.empty())){
+            drawInfoPnl();
         }
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        String text = searchFld.getText();
+        if(!(text.isEmpty())){
+            motherFrm.filter.unckeckAll();
+            infoPnlStack.clear();
+            //NaverMap2.map_service(motherFrm, Long.parseLong(text));
+            AttractionInfo attractionInfo = new AttractionInfo(this,DataBase.getCourseDataList(Long.parseLong(text)).get(1));
+            pushinfoPnl(attractionInfo);
+        }
+    }
+
 }
