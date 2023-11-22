9a3cf996e50219194dcd1fcdd2e7eca5249d1add
Lee Gyeongju <rudwn969@naver.com>
2023-11-23 00:45:04 +0900

    필터를 통한 코스들 검색 테마만 구현

--- src/main/java/org/howWeather/Filter.java
+++ src/main/java/org/howWeather/Filter.java
@@ -2,13 +2,18 @@ package org.howWeather;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.desktop.SystemEventListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Filter extends JPanel {
+public class Filter extends JPanel implements ActionListener {
 
     Frame motherfrm;
-    FilterGroup[] filterGroups = new FilterGroup[3];
+    FilterGroup themeGrp;
+    FilterGroup locateGrp;
+    FilterGroup weatherGrp;
 
     public Filter(Frame frm){
         motherfrm = frm;
@@ -20,28 +25,47 @@ public class Filter extends JPanel {
         setBackground(Color.white);
         setPreferredSize(new Dimension(300,750));
 
-        filterGroups[0] = new FilterGroup("테마",new String[]{"문화/예술","쇼핑/놀이","자연/힐링","종교/역사/전통","체험/학습/산업","캠핑/스포츠"});
-        filterGroups[1] = new FilterGroup("지역",new String[]{"수도권","강원","충청","경상","전라"});
-        filterGroups[2] = new FilterGroup("날씨",new String[]{"맑음","비","흐림"});
-        add(filterGroups[0]);
-        add(filterGroups[1]);
-        add(filterGroups[2]);
+        locateGrp = new FilterGroup("지역",new String[]{"수도권","강원","충청","경상","전라"});
+        themeGrp = new FilterGroup("테마",new String[]{"문화/예술","쇼핑/놀이","자연/힐링","종교/역사/전통","체험/학습/산업","캠핑/스포츠"});
+        weatherGrp = new FilterGroup("날씨",new String[]{"맑음","비","흐림"});
+
+        add(locateGrp);
+        add(themeGrp);
+        add(weatherGrp);
 
         JButton applyBtn = new JButton("필터 적용");
-        applyBtn.addActionListener(new FilterEvent());
+        applyBtn.addActionListener(this);
         add(applyBtn);
     }
 
     public void unckeckAll(){
-        for(FilterGroup filtergroup: filterGroups){
-            filtergroup.uncheckAll();
-        }
+       locateGrp.uncheckAll();
+       themeGrp.uncheckAll();
+       weatherGrp.uncheckAll();
     }
 
-    private class FilterEvent implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e){
+    private void printThemeCourse(String theme) {
+        // 여기서 CourseData 리스트는 DataBase 클래스에서 가져옵니다.
+        List<CourseData> courseList = DataBase.getAllCourseData();
+        List<List <CourseData>> resultList = new ArrayList<>();
+        for (CourseData courseData : courseList) {
+            if (courseData.getThemeName().equals(theme)) {
+                Long ID = courseData.getCourseId();
+                List<CourseData> temp = DataBase.getCourseDataList(ID);
+                if(!(resultList.contains(temp))){
+                    resultList.add(temp);
+                }
+            }
+        }
+        motherfrm.search.pushinfoPnl(new CourseInfo(motherfrm.search, resultList));
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e){
+        for (JCheckBox checkBox : themeGrp.checkBoxes) {
+            if (checkBox.isSelected()) {
+                printThemeCourse(checkBox.getText());
+            }
         }
     }
 }
