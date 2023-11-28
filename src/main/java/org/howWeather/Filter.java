e78538887db825d4ab0b88d81325ed48ce5dc855
Han chulhee <myhan601@naver.com>
2023-11-29 00:34:43 +0900

    printRegionCourse 메서드 수정

--- src/main/java/org/howWeather/Filter.java
+++ src/main/java/org/howWeather/Filter.java
@@ -145,39 +145,14 @@ public class Filter extends JPanel implements ActionListener {
     }
 
     private void printRegionCourse(String region) {
-        // 지역에 따른 코드를 설정
-        List<String> regionCodes = new ArrayList<>();
-        switch (region) {
-            case "서울 특별시":
-                regionCodes.add("11");
-                break;
-            case "경기도":
-                regionCodes.add("41");
-                break;
-            case "강원도":
-                regionCodes.add("42");
-                break;
-            case "충청도":
-                regionCodes.add("43");
-                regionCodes.add("44");
-                break;
-            case "전라도":
-                regionCodes.add("45");
-                regionCodes.add("46");
-                break;
-            case "경상도":
-                regionCodes.add("47");
-                regionCodes.add("48");
-                break;
-
-        }
-
-        // 지역 코드를 사용하여 데이터베이스에서 정보 조회
-        for (String regionCode : regionCodes) {
+        String regionCode = regionCodeMap.get(region);
+        if (regionCode != null) {
             List<CourseData> courseList = DataBase.getCourseDataByRegion(regionCode);
             for (CourseData courseData : courseList) {
                 System.out.println(courseData.toString());
             }
+        } else {
+            System.out.println("해당 지역의 코드를 찾을 수 없습니다.");
         }
     }
 
