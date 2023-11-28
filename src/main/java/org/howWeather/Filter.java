b3eddc713fcdfc701f84a179579775df2a48376b
coolman0123 <115522070+coolman0123@users.noreply.github.com>
2023-11-28 11:52:56 +0900

    filter-수정사항 반영

--- src/main/java/org/howWeather/Filter.java
+++ src/main/java/org/howWeather/Filter.java
@@ -5,8 +5,9 @@ import java.awt.*;
 import java.awt.desktop.SystemEventListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Filter extends JPanel implements ActionListener {
 
@@ -15,19 +16,19 @@ public class Filter extends JPanel implements ActionListener {
     FilterGroup locateGrp;
     FilterGroup weatherGrp;
 
-    public Filter(Frame frm){
+    public Filter(Frame frm) {
         motherfrm = frm;
         VisibleFilter();
     }
 
-    public void VisibleFilter(){
+    public void VisibleFilter() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBackground(Color.white);
-        setPreferredSize(new Dimension(300,750));
+        setPreferredSize(new Dimension(300, 750));
 
-        locateGrp = new FilterGroup("지역",new String[]{"수도권","강원","충청","경상","전라"});
-        themeGrp = new FilterGroup("테마",new String[]{"문화/예술","쇼핑/놀이","자연/힐링","종교/역사/전통","체험/학습/산업","캠핑/스포츠"});
-        weatherGrp = new FilterGroup("날씨",new String[]{"맑음","비","흐림"});
+        locateGrp = new FilterGroup("지역", new String[]{"수도권", "강원", "충청", "경상", "전라"});
+        themeGrp = new FilterGroup("테마", new String[]{"문화/예술", "쇼핑/놀이", "자연/힐링", "종교/역사/전통", "체험/학습/산업", "캠핑/스포츠"});
+        weatherGrp = new FilterGroup("날씨", new String[]{"맑음", "비", "흐림"});
 
         add(locateGrp);
         add(themeGrp);
@@ -38,34 +39,56 @@ public class Filter extends JPanel implements ActionListener {
         add(applyBtn);
     }
 
-    public void unckeckAll(){
-       locateGrp.uncheckAll();
-       themeGrp.uncheckAll();
-       weatherGrp.uncheckAll();
+    public void unckeckAll() {
+        locateGrp.uncheckAll();
+        themeGrp.uncheckAll();
+        weatherGrp.uncheckAll();
     }
 
-    private void printThemeCourse(String theme) {
-        // 여기서 CourseData 리스트는 DataBase 클래스에서 가져옵니다.
+    private void printThemeCourse(List<String> themes) {
         List<CourseData> courseList = DataBase.getAllCourseData();
-        List<List <CourseData>> resultList = new ArrayList<>();
+        List<List<CourseData>> resultList = new ArrayList<>();
+        Map<Long, List<CourseData>> courseDataMap = new HashMap<>();
+
         for (CourseData courseData : courseList) {
-            if (courseData.getThemeName().equals(theme)) {
-                Long ID = courseData.getCourseId();
-                List<CourseData> temp = DataBase.getCourseDataList(ID);
-                if(!(resultList.contains(temp))){
-                    resultList.add(temp);
-                }
+            Long ID = courseData.getCourseId();
+            if (!courseDataMap.containsKey(ID)) {
+                courseDataMap.put(ID, DataBase.getCourseDataList(ID));
             }
         }
+        resultList.addAll(courseDataMap.values());
+
+        for (Iterator<List<CourseData>> iterator = resultList.iterator(); iterator.hasNext(); ) {
+            List<CourseData> courseDataList = iterator.next();
+            boolean isFiltered = true;
+
+            for (String theme : themes) {
+                if (courseDataList.stream().anyMatch(courseData -> courseData.getThemeName().equals(theme))) {
+                    isFiltered = false;
+                } else {
+                    isFiltered = true;
+                    break;
+                }
+            }
+
+            if (isFiltered) {
+                iterator.remove();
+            }
+        }
+
         motherfrm.search.pushinfoPnl(new CourseInfo(motherfrm.search, resultList));
     }
 
+
     @Override
-    public void actionPerformed(ActionEvent e){
+    public void actionPerformed(ActionEvent e) {
+        List<String> selectedThemes = new ArrayList<>();
         for (JCheckBox checkBox : themeGrp.checkBoxes) {
             if (checkBox.isSelected()) {
-                printThemeCourse(checkBox.getText());
+                selectedThemes.add(checkBox.getText());
             }
         }
+        printThemeCourse(selectedThemes);
     }
 }
+
